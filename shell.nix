7ec916c86585c25d5f4312c3e2fd36e09a9c52a1
Marcin Kuzminski <marcin@rhodecode.com>
2016-11-10 17:19:23 +0100

    requirements: added ipdb (0.10.1) support.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,11 @@
 { pkgs ? import <nixpkgs> {}
-,  doCheck ? false
+, doCheck ? false
 }:
 
 let
+
   vcsserver = import ./default.nix {
-    inherit
-      doCheck
-      pkgs;
+    inherit pkgs doCheck;
   };
 
   vcs-pythonPackages = vcsserver.pythonPackages;
@@ -16,6 +15,12 @@ in vcsserver.override (attrs: {
   # Avoid that we dump any sources into the store when entering the shell and
   # make development a little bit more convenient.
   src = null;
+
+  buildInputs =
+    attrs.buildInputs ++
+    (with vcs-pythonPackages; [
+      ipdb
+    ]);
 
   # Somewhat snappier setup of the development environment
   # TODO: think of supporting a stable path again, so that multiple shells
