713224b48cc0b875fa863cb2213688d03beff31b
Marcin Kuzminski <marcin@rhodecode.com>
2018-06-28 11:54:03 +0200

    nix: updated to 18.03 packages.
    - new nix structure
    - adjusted build problems
    - new generated python-packages.nix
    - drop setuptools
    - release.nix update

--- pkgs/python-packages-overrides.nix
+++ pkgs/python-packages-overrides.nix
@@ -4,58 +4,49 @@
 # python-packages.nix. The main objective is to add needed dependencies of C
 # libraries and tweak the build instructions where needed.
 
-{ pkgs, basePythonPackages }:
+{ pkgs
+, basePythonPackages
+}:
 
 let
   sed = "sed -i";
+
 in
 
 self: super: {
 
-  beaker = super.beaker.override (attrs: {
+  "beaker" = super."beaker".override (attrs: {
     patches = [
-      ./patch-beaker-lock-func-debug.diff
-      ./patch-beaker-metadata-reuse.diff
+      ./patch_beaker/patch-beaker-lock-func-debug.diff
+      ./patch_beaker/patch-beaker-metadata-reuse.diff
     ];
   });
 
-  subvertpy = super.subvertpy.override (attrs: {
-    # TODO: johbo: Remove the "or" once we drop 16.03 support
-    SVN_PREFIX = "${pkgs.subversion.dev or pkgs.subversion}";
+  "hgsubversion" = super."hgsubversion".override (attrs: {
     propagatedBuildInputs = attrs.propagatedBuildInputs ++ [
+      pkgs.sqlite
+      #basePythonPackages.sqlite3
+      self.mercurial
+    ];
+  });
+
+  "subvertpy" = super."subvertpy".override (attrs: {
+    SVN_PREFIX = "${pkgs.subversion.dev}";
+    propagatedBuildInputs = [
+      pkgs.apr.dev
       pkgs.aprutil
       pkgs.subversion
     ];
-    preBuild = pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
-      ${sed} -e "s/'gcc'/'clang'/" setup.py
-    '';
   });
 
-  hgsubversion = super.hgsubversion.override (attrs: {
-    propagatedBuildInputs = attrs.propagatedBuildInputs ++ [
-      pkgs.sqlite
-      basePythonPackages.sqlite3
+  "mercurial" = super."mercurial".override (attrs: {
+    propagatedBuildInputs = [
+      # self.python.modules.curses
     ];
   });
 
-  mercurial = super.mercurial.override (attrs: {
-    propagatedBuildInputs = attrs.propagatedBuildInputs ++ [
-      self.python.modules.curses
-    ] ++ pkgs.lib.optional pkgs.stdenv.isDarwin
-      pkgs.darwin.apple_sdk.frameworks.ApplicationServices;
-  });
-
-  pyramid = super.pyramid.override (attrs: {
-    postFixup = ''
-      wrapPythonPrograms
-      # TODO: johbo: "wrapPython" adds this magic line which
-      # confuses pserve.
-      ${sed} '/import sys; sys.argv/d' $out/bin/.pserve-wrapped
-    '';
-  });
-
-  # Avoid that setuptools is replaced, this leads to trouble
-  # with buildPythonPackage.
-  setuptools = basePythonPackages.setuptools;
+  # Avoid that base packages screw up the build process
+  inherit (basePythonPackages)
+    setuptools;
 
 }
