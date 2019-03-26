6ff7ed66fb01aae8284f2f2bf5b6372b9c5f0fc8
Marcin Kuzminski <marcin@rhodecode.com>
2019-03-26 14:36:00 +0100

    dependencies: bumped webtest to 2.0.33

--- pkgs/python-packages.nix
+++ pkgs/python-packages.nix
@@ -897,7 +897,7 @@ self: super: {
     };
   };
   "webtest" = super.buildPythonPackage {
-    name = "webtest-2.0.32";
+    name = "webtest-2.0.33";
     doCheck = false;
     propagatedBuildInputs = [
       self."six"
@@ -906,8 +906,8 @@ self: super: {
       self."beautifulsoup4"
     ];
     src = fetchurl {
-      url = "https://files.pythonhosted.org/packages/27/9f/9e74449d272ffbef4fb3012e6dbc53c0b24822d545e7a33a342f80131e59/WebTest-2.0.32.tar.gz";
-      sha256 = "0qp0nnbazzm4ibjiyqfcn6f230svk09i4g58zg2i9x1ga06h48a2";
+      url = "https://files.pythonhosted.org/packages/a8/b0/ffc9413b637dbe26e291429bb0f6ed731e518d0cd03da28524a8fe2e8a8f/WebTest-2.0.33.tar.gz";
+      sha256 = "1l3z0cwqslsf4rcrhi2gr8kdfh74wn2dw76376i4g9i38gz8wd21";
     };
     meta = {
       license = [ pkgs.lib.licenses.mit ];
