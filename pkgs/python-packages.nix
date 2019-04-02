6c082835ab8adc64d488d6c95758d84f341b0ae9
Marcin Kuzminski <marcin@rhodecode.com>
2019-04-02 15:35:50 +0000

    release: updated pip2nix output for 4.16.2

--- pkgs/python-packages.nix
+++ pkgs/python-packages.nix
@@ -658,7 +658,7 @@ self: super: {
     };
   };
   "rhodecode-vcsserver" = super.buildPythonPackage {
-    name = "rhodecode-vcsserver-4.16.1";
+    name = "rhodecode-vcsserver-4.16.2";
     buildInputs = [
       self."pytest"
       self."py"
