584c9e6c46e32d95748b0f29d59c169e28ec120f
Marcin Kuzminski <marcin@rhodecode.com>
2020-04-16 00:31:07 +0200

    code: update copyrights to 2020

--- vcsserver/hook_utils/__init__.py
+++ vcsserver/hook_utils/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # RhodeCode VCSServer provides access to different vcs backends via network.
-# Copyright (C) 2014-2019 RhodeCode GmbH
+# Copyright (C) 2014-2020 RhodeCode GmbH
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
